d844175c3eee4e916b8c9d0b4d96350c4d01cee5
wangchangzu <15895903773@163.com>
2018-04-18 11:45:18 +0800

    '上午环境配置'

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,53 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'starter',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    title: '简书',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        name: 'author',
+        content: '王昌祖'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: '我自己的简书'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   /*
-  ** Global CSS
-  */
-  css: ['~/assets/css/main.css'],
+   ** Global CSS
+   */
+  css: ['~/assets/css/main.css','~/assets/css/animate.css',"~/assets/css/normalize.css","~/assets/css/font-awesome.css"],
   /*
-  ** Add axios globally
-  */
+   ** Add axios globally
+   */
   build: {
     vendor: ['axios'],
     /*
-    ** Run ESLINT on save
-    */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+     ** Run ESLINT on save
+     */
+    // extend (config, ctx) {
+    //   if (ctx.isClient) {
+    //     config.module.rules.push({
+    //       enforce: 'pre',
+    //       test: /\.(js|vue)$/,
+    //       loader: 'eslint-loader',
+    //       exclude: /(node_modules)/
+    //     })
+    //   }
+    // }
   }
 }
