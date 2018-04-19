704b3371be6791551f0e1962ac2e2f6227dd465e
damowang666 <1690696646@qq.com>
2018-04-19 21:45:24 +0800

    完成登录、注册，导航条部分功能

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
+  <div id="default-template">
     <nuxt/>
   </div>
 </template>
-
 <script>
-
 export default {
-
 }
 </script>
+<style>
+  body {
+    padding-top:56px;
+    min-width:768px;
+    font-size:17px;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,30 @@
 <template>
-    <div>
-        <p>hello world</p>
+    <div style="height:100%">
+       <my-header></my-header>
+        <p>我的主体内容部分</p>
     </div>
 </template>
-
-
 <script>
-export default {
-    data(){
-        return{
-            name:'首页'
+    import myHeader from  '../components/myHeader'
+    export default {
+        head:{
+            title:'简书-创作你的创作',
+            meta:[
+                {charset:'utf-8'},
+                {name:'keywords',content:'简书首页'},
+                {name:'description',content:'简书首页'},
+                {name:'author',content:'李志远'}
+            ]
+        },
+        components:{
+            myHeader
+        },
+        data () {
+            return {
+                name:'首页'
+            }
         }
     }
-}
 </script>
-
 <style>
-
 </style>
